d205f5df0564132aadb0074e9517d30f042b0567
AmalSaqib <amal.saqib2001@gmail.com>
2024-05-07 20:02:34 +0300

    new data

---
+++ count.py
@@ -0,0 +1,10 @@
+import os
+
+input_folder = "./dataset/train"
+
+
+for item in os.listdir(input_folder):
+    folder = os.path.join(input_folder, item)
+    no_of_files = len(os.listdir(folder))
+    
+    print(item, no_of_files)        
